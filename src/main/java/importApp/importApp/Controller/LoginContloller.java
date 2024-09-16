15e833fea0e5a1b7804f41d47979fc2950ce124f
T.H(kaminuma) <moshimo_0817@yahoo.co.jp>
2024-09-16 21:31:17 +0900

    APIとUIリポジトリ分けの為の一時Spring-securityの非活性

--- src/main/java/importApp/importApp/Controller/LoginContloller.java
+++ src/main/java/importApp/importApp/Controller/LoginContloller.java
@@ -1,45 +1,45 @@
-package importApp.importApp.Controller;
-
-import importApp.importApp.Entity.UserEntity;
-import importApp.importApp.Security.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import javax.servlet.http.HttpSession;
-
-@Controller
-public class LoginContloller {
-
-    private final UserService userService;
-
-    @Autowired
-    public LoginContloller(UserService userService) {
-        this.userService = userService;
-    }
-
-    @GetMapping("/login")
-    public String login() {
-        return "login"; // ログインページのビュー名
-    }
-    @PostMapping("/login")
-    public String loginPost(
-            @RequestParam("username") String username,
-            @RequestParam("password") String password) {
-
-        boolean isValidUser = userService.isUserValid(username, password);
-
-            // ユーザー認証の処理を実行
-
-        if (isValidUser) {
-            // ログイン成功時の処理
-            return "redirect:/"; // ログイン成功後にリダイレクトするURL
-        } else {
-            // ログイン失敗時の処理
-            return "redirect:/login"; // ログイン失敗後にリダイレクトするURL
-        }
-    }
-}
+//package importApp.importApp.Controller;
+//
+//import importApp.importApp.Entity.UserEntity;
+//import importApp.importApp.Security.UserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+//
+//import javax.servlet.http.HttpSession;
+//
+//@Controller
+//public class LoginContloller {
+//
+//    private final UserService userService;
+//
+//    @Autowired
+//    public LoginContloller(UserService userService) {
+//        this.userService = userService;
+//    }
+//
+//    @GetMapping("/login")
+//    public String login() {
+//        return "login"; // ログインページのビュー名
+//    }
+//    @PostMapping("/login")
+//    public String loginPost(
+//            @RequestParam("username") String username,
+//            @RequestParam("password") String password) {
+//
+//        boolean isValidUser = userService.isUserValid(username, password);
+//
+//            // ユーザー認証の処理を実行
+//
+//        if (isValidUser) {
+//            // ログイン成功時の処理
+//            return "redirect:/"; // ログイン成功後にリダイレクトするURL
+//        } else {
+//            // ログイン失敗時の処理
+//            return "redirect:/login"; // ログイン失敗後にリダイレクトするURL
+//        }
+//    }
+//}
--- src/main/java/importApp/importApp/Service/UserDetailsServiceImpl.java
+++ src/main/java/importApp/importApp/Service/UserDetailsServiceImpl.java
@@ -1,25 +1,25 @@
-package importApp.importApp.Service;
-
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserDetailsServiceImpl implements UserDetailsService {
-
-    // ユーザーデータベースからユーザー情報を検索
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // データベースからユーザーを取得するコードを記述
-
-        // 例: ダミーのユーザーを作成
-        UserDetails user = User.withUsername(username)
-                .password("{noop}password") // パスワード（ハッシュ化しない場合は{noop}を前置き）
-                .roles("USER")
-                .build();
-
-        return user;
-    }
-}
+//package importApp.importApp.Service;
+//
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class UserDetailsServiceImpl implements UserDetailsService {
+//
+//    // ユーザーデータベースからユーザー情報を検索
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        // データベースからユーザーを取得するコードを記述
+//
+//        // 例: ダミーのユーザーを作成
+//        UserDetails user = User.withUsername(username)
+//                .password("{noop}password") // パスワード（ハッシュ化しない場合は{noop}を前置き）
+//                .roles("USER")
+//                .build();
+//
+//        return user;
+//    }
+//}

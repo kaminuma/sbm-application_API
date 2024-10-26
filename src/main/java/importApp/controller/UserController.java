3ce6172bc841834759c0f086da2be4ff7260db29
T.H(kaminuma) <moshimo_0817@yahoo.co.jp>
2024-10-26 20:51:44 +0900

    ログインロジックと新規登録ロジックの追記

--- src/main/java/importApp/controller/UserController.java
+++ src/main/java/importApp/controller/UserController.java
@@ -1,23 +1,36 @@
 package importApp.controller;
 
 import importApp.entity.UserEntity;
+import importApp.model.LoginRequest;
+import importApp.model.UserResponse;
 import importApp.service.UserService;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/auth")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @PostMapping("/register")
-    public void registerUser(@RequestBody UserEntity user) {
+    public ResponseEntity<?> register(@RequestBody UserEntity user) {
         userService.registerUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        UserEntity user = userService.loginUser(loginRequest.getUsername(), loginRequest.getPassword());
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+        }
+
+        return ResponseEntity.ok(new UserResponse(user.getUser_id(), user.getUsername()));
     }
 }
 
--- src/main/java/importApp/mapper/UserMapper.java
+++ src/main/java/importApp/mapper/UserMapper.java
@@ -7,4 +7,6 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface UserMapper {
     void insertUser(UserEntity user);
+    UserEntity findByEmail(String email);
+    UserEntity findByUsername(String username);
 }

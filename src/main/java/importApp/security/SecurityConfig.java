45efed06539e45bf6b5e6a776caae27371ead0dc
T.H(kaminuma) <moshimo_0817@yahoo.co.jp>
2024-10-30 22:37:45 +0900

    Spring-Securityのjwt関連の追記

--- src/main/java/importApp/security/SecurityConfig.java
+++ src/main/java/importApp/security/SecurityConfig.java
@@ -1,15 +1,32 @@
 package importApp.security;
 
+import importApp.filter.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+<<<<<<< Updated upstream
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+=======
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+>>>>>>> Stashed changes
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
+        return new JwtAuthenticationFilter(authenticationManagerBean());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -19,14 +36,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated() // それ以外は認証が必要
                 .and()
                 .formLogin().disable() // フォームログインを無効化
-                .httpBasic().disable();
+                .httpBasic().disable()
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
+
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+<<<<<<< Updated upstream
     @Configuration
     public static class WebConfig implements WebMvcConfigurer {
 
@@ -38,5 +58,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .allowedHeaders("*")
                     .allowCredentials(true);
         }
+=======
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+>>>>>>> Stashed changes
     }
 }

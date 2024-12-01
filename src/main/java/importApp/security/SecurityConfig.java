d1dd8eeeba5bc3b59f9cc6fb67203ff685e96cda
T.H(kaminuma) <moshimo_0817@yahoo.co.jp>
2024-12-01 21:42:16 +0900

    許可IPの変更

--- src/main/java/importApp/security/SecurityConfig.java
+++ src/main/java/importApp/security/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**") // すべてのエンドポイントに適用
-                    .allowedOrigins("http://localhost:5173","http://13.211.29.238")
+                    .allowedOrigins("http://localhost:5173","http://3.105.133.253")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);

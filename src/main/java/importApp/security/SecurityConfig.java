96499dcd2f5a8d8be7b04fb331611aa34eb3226f
T.H(kaminuma) <moshimo_0817@yahoo.co.jp>
2024-12-02 15:00:09 +0900

    test

--- src/main/java/importApp/security/SecurityConfig.java
+++ src/main/java/importApp/security/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**") // すべてのエンドポイントに適用
-                    .allowedOrigins("http://localhost:5173", "https://sbm-app.com", "https://www.sbm-app.com")
+                    .allowedOrigins("*")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);

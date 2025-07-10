f24b5d718b76d202450fa09858d214eb80e0521d
kaminuma <>
2025-07-10 19:07:07 +0900

    fix

--- src/main/java/importApp/security/SecurityConfig.java
+++ src/main/java/importApp/security/SecurityConfig.java
@@ -51,8 +51,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**") // すべてのエンドポイントに適用
-                    //　.allowedOrigins("http://localhost:5173", "https://sbm-app.com", "https://www.sbm-app.com") // 本番用設定
-                    .allowedOriginPatterns("*") // 開発テスト用：全オリジンを許可
+                    .allowedOrigins("http://localhost:5173", "https://sbm-app.com", "https://www.sbm-app.com") // 本番用設定
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);
